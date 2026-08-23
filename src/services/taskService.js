8cad05e46614f2af95eb373987eb23dc1defd440
lochanajayasinghe2006 <lochanajayasinghe006@gmail.com>
2026-08-23 09:19:03 +0530

    Add service folder & Style Folder Files

---
+++ src/services/taskService.js
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export const getTasks = async (boardId) => {
+  const response = await api.get(`/tasks?boardId=${boardId}`);
+  return response.data;
+};
+
+export const createTask = async (taskData) => {
+  const response = await api.post("/tasks", taskData);
+  return response.data;
+};
+
+export const updateTask = async (taskId, taskData) => {
+  const response = await api.put(`/tasks/${taskId}`, taskData);
+  return response.data;
+};
+
+export const deleteTask = async (taskId) => {
+  const response = await api.delete(`/tasks/${taskId}`);
+  return response.data;
+};
