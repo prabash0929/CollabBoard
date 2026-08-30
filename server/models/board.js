deed6621f476230c626e205e4fb1a45a9a8a1fb6
tharushinethmini429-alt <tharushinethmini429@gmail.com>
2026-08-30 10:11:23 +0530

    Add models folder board file

---
+++ server/models/board.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const boardSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Board", boardSchema);
